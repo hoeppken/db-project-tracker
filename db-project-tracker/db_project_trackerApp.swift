2ca050645ee0cf1ced4667c9a4b9f7ed21fa1318
hoeppken <rafael_rsv@hotmail.com>
2025-06-12 07:00:49 +0200

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Rafael dos Santos Varela on 12.06.25.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
